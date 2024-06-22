fb0c26d535501979b0d2a8eb9df48f6c9b29b8a8
turtureanu <90131332+turtureanu@users.noreply.github.com>
2024-06-22 14:56:01 +0200

    deps: get rid of unused packages + migrate to bun

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The Knowleaks Blog';
-export const SITE_DESCRIPTION = "Sharing things I might forget. Knowleaks is sort of a blog, but at the same time it's also a nicely decorated notebook for everything that I might need in the future.";
+export const SITE_TITLE = "The Knowleaks Blog";
+export const SITE_DESCRIPTION =
+  "Sharing things I might forget. A blog about the things I need to remind myself.";
