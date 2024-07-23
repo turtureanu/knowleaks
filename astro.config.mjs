105c892f02aa12eb1d7a9cfb33f2d0e524f463c8
turtureanu <90131332+turtureanu@users.noreply.github.com>
2024-07-23 19:06:56 +0200

    feat(UX): migrate from OrigamiCarousel to Swiper
    Change font-size of some elements (blockquote, .code)

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,4 +33,7 @@ export default defineConfig({
     },
   },
   integrations: [mdx(), sitemap(), compress(), robotsTxt(), svelte()],
+  optimizeDeps: {
+    include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
+  },
 });
