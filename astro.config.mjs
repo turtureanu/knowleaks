2ad0604aea5b3fb4d3c394a80434fd496ae20aca
turtureanu <90131332+turtureanu@users.noreply.github.com>
2025-01-24 13:09:08 +0100

    fix: inline code not showing up
    deps: update to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,46 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import robotsTxt from 'astro-robots-txt';
-import purgeCSS from 'astro-purgecss';
+import robotsTxt from "astro-robots-txt";
+import purgeCSS from "astro-purgecss";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://knowleaks.netlify.app',
-	markdown: {
-		shikiConfig: {
-			// Choose from Shiki"s built-in themes (or add your own)
-			// https://shiki.style/themes
-			// Alternatively, provide multiple themes
-			// See note below for using dual light/dark themes
-			themes: {
-				light: 'github-light',
-				dark: 'nord'
-			},
-			// Disable the default colors
-			// https://shiki.style/guide/dual-themes#without-default-color
-			// (Added in v4.12.0)
-			// Add custom languages
-			defaultColor: false,
-			// Note: Shiki has countless langs built-in, including .astro!
-			// https://shiki.style/languages
-			langs: []
-		}
-	},
-	integrations: [
-		mdx(),
-		sitemap(),
-		robotsTxt(),
-		purgeCSS({
-			content: ['./src/**/*.{astro,svelte,vue,tsx,jsx,ts,js}']
-		}),
-		svelte()
-	],
-	optimizeDeps: {
-		include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
-	}
+  site: "https://knowleaks.netlify.app",
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki"s built-in themes (or add your own)
+      // https://shiki.style/themes
+      // Alternatively, provide multiple themes
+      // See note below for using dual light/dark themes
+      themes: {
+        light: "github-light",
+        dark: "nord",
+      },
+      // Disable the default colors
+      // https://shiki.style/guide/dual-themes#without-default-color
+      // (Added in v4.12.0)
+      // Add custom languages
+      defaultColor: false,
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+    },
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    robotsTxt(),
+    purgeCSS({
+      content: ["./src/**/*.{astro,svelte,vue,tsx,jsx,ts,js}"],
+    }),
+    svelte(),
+  ],
+  optimizeDeps: {
+    include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
+  },
 });
