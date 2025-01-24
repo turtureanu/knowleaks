608e4263ea632e2cd66e8e30fbbd56a6e5c4b714
turtureanu <90131332+turtureanu@users.noreply.github.com>
2025-01-24 13:10:07 +0100

    fix: switch to knowleaks.org domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://knowleaks.netlify.app",
+  site: "https://knowleaks.org",
   markdown: {
     shikiConfig: {
       // Choose from Shiki"s built-in themes (or add your own)
