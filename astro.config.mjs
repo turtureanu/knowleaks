13909417ef563cf0e022cb2c2781489de3cd4150
Mircea-Sebastian Turtureanu <90131332+turtureanu@users.noreply.github.com>
2026-03-05 15:28:58 +0100

    feat: migrate to Biome + Stylelint
    remove nixpacks.toml and Prettier
    
    Turns out the problem with the green blocks forming on the .avif images
    is caused by building on arm64, this an open issue on sharp
    (#4491) about this, so I'm waiting for the fix.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,50 +1,46 @@
 import mdx from "@astrojs/mdx";
-import astroCompress from "gab-astro-compress";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 import purgeCSS from "astro-purgecss";
 // https://astro.build/config
 import robotsTxt from "astro-robots-txt";
+import astroCompress from "gab-astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://knowleaks.org",
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki"s built-in themes (or add your own)
-      // https://shiki.style/themes
-      // Alternatively, provide multiple themes
-      // See note below for using dual light/dark themes
-      themes: {
-        light: "github-light",
-        dark: "nord",
-      },
-      // Disable the default colors
-      // https://shiki.style/guide/dual-themes#without-default-color
-      // (Added in v4.12.0)
-      // Add custom languages
-      defaultColor: false,
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
-      langs: [],
-    },
-  },
-  integrations: [
-    mdx(),
-    sitemap(),
-    robotsTxt(),
-    purgeCSS({
-      content: ["./src/**/*.{astro,svelte,vue,tsx,jsx,ts,js}"],
-    }),
-    svelte(),
-    astroCompress({
-      avif: {
-        quality: 80,
-      },
-    }),
-  ],
-  optimizeDeps: {
-    include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
-  },
+	site: "https://knowleaks.org",
+	markdown: {
+		shikiConfig: {
+			// Choose from Shiki"s built-in themes (or add your own)
+			// https://shiki.style/themes
+			// Alternatively, provide multiple themes
+			// See note below for using dual light/dark themes
+			themes: {
+				light: "github-light",
+				dark: "nord",
+			},
+			// Disable the default colors
+			// https://shiki.style/guide/dual-themes#without-default-color
+			// (Added in v4.12.0)
+			// Add custom languages
+			defaultColor: false,
+			// Note: Shiki has countless langs built-in, including .astro!
+			// https://shiki.style/languages
+			langs: [],
+		},
+	},
+	integrations: [
+		mdx(),
+		sitemap(),
+		robotsTxt(),
+		purgeCSS({
+			content: ["./src/**/*.{astro,svelte,vue,tsx,jsx,ts,js}"],
+		}),
+		svelte(),
+		astroCompress(),
+	],
+	optimizeDeps: {
+		include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
+	},
 });
